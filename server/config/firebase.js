c05a473b6f0e2fab694d3aca0060d12ab58b2ad2
Kyle <kylekhezierdaugduag@gmail.com>
2024-09-13 11:05:09 +0800

    remove server

--- server/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../keys/productKey');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
--- server/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Add user-related routes here
-
-module.exports = router;
