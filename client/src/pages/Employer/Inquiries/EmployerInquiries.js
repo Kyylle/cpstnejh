3cfcb259a8cb1564afb8590a406a6dd76606feee
ㅤKyle <63981@DevK.kairu>
2024-09-23 22:33:26 +0800

    Inquiries component added

---
+++ client/src/pages/Employer/Inquiries/EmployerInquiries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+import './Inquiries.css'
+ const EmployerInquiries = () => {
+    return (
+        <div className='inquiries-navbar'>
+            <Navigation/>
+        </div>
+    )
+}
+
+export default EmployerInquiries;
